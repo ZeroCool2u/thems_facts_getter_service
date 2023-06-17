898ac12ab21e607a8b36d297980317ac6b2aeb0d
Theo Linnemann <theo@theolinnemann.com>
2023-06-16 23:09:40 -0400

    Replace UJSON with ORJSON

--- fact_getter.py
+++ fact_getter.py
@@ -4,11 +4,11 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from google.cloud import firestore
 from google.cloud.exceptions import NotFound
-from starlette.responses import UJSONResponse
+from orjson import loads
 from starlette.status import HTTP_200_OK
-from ujson import loads
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%m-%d-%Y %I:%M:%S %p', level=logging.WARNING)
@@ -47,63 +47,63 @@ app = FastAPI(title='Fact Getter',
                           'into a single unified interface.')
 
 
-@app.get("/cat", response_class=UJSONResponse)
+@app.get("/cat", response_class=ORJSONResponse)
 async def cat_fact():
     r = requests.get(url='https://catfact.ninja/fact')
     return {'fact': loads(r.content)['fact']}
 
 
-@app.get("/kanye", response_class=UJSONResponse)
+@app.get("/kanye", response_class=ORJSONResponse)
 async def kanye_fact():
     r = requests.get(url='https://api.kanye.rest')
     return {'fact': loads(r.content)['quote']}
 
 
-@app.get("/inspirational", response_class=UJSONResponse)
+@app.get("/inspirational", response_class=ORJSONResponse)
 async def inspirational_quote():
     r = requests.get(url='https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
     return {'fact': loads(r.content)['quoteText']}
 
 
-@app.get("/design", response_class=UJSONResponse)
+@app.get("/design", response_class=ORJSONResponse)
 async def design_quote():
     r = requests.get(url='https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand')
     soup = BeautifulSoup(loads(r.content)[0]['content']['rendered'], 'lxml')
     return {'fact': soup.get_text(strip=True)}
 
 
-@app.get("/simpsons", response_class=UJSONResponse)
+@app.get("/simpsons", response_class=ORJSONResponse)
 async def simpsons_quote():
     r = requests.get(url='https://thesimpsonsquoteapi.glitch.me/quotes')
     return {'fact': loads(r.content)[0]['quote']}
 
 
-@app.get("/swanson", response_class=UJSONResponse)
+@app.get("/swanson", response_class=ORJSONResponse)
 async def swanson_quote():
     r = requests.get(url='http://ron-swanson-quotes.herokuapp.com/v2/quotes')
     return {'fact': loads(r.content)[0]}
 
 
-@app.get("/norris", response_class=UJSONResponse)
+@app.get("/norris", response_class=ORJSONResponse)
 async def chuck_norris_fact():
     r = requests.get(url='https://api.chucknorris.io/jokes/random')
     return {'fact': loads(r.content)['value']}
 
 
-@app.get("/shitty-trump", response_class=UJSONResponse)
+@app.get("/shitty-trump", response_class=ORJSONResponse)
 async def trump_quote():
     r = requests.get(url='https://api.tronalddump.io/random/quote')
     return {'fact': loads(r.content)['value']}
 
 
-@app.get("/random_gif", response_class=UJSONResponse)
+@app.get("/random_gif", response_class=ORJSONResponse)
 async def rand_gif():
     giphy_api_key: str = API_KEYS['giphy_api_key']
     r = requests.get(url=f"http://api.giphy.com/v1/gifs/trending?api_key={giphy_api_key}&limit=1")
     return {'fact': loads(r.content)['data'][0]['images']['looping']['mp4']}
 
 
-@app.get("/dad_joke", response_class=UJSONResponse)
+@app.get("/dad_joke", response_class=ORJSONResponse)
 async def rand_dad():
     r = requests.get("https://icanhazdadjoke.com/", headers={'Accept': 'application/json'})
     return {'fact': loads(r.content)['joke']}
